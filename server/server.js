3392e902140bc15d2678f70a6469e1e0197fa8c1
Ivan <ivanms1@gmail.com>
2018-04-25 22:35:28 +0900

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { ObjectId } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -31,6 +32,21 @@ app.get('/todos', (req, res) => {
 	}, (err) => {
 		res.status(400).send(err);
 	})
+});
+
+app.get('/todos/:id', (req, res) => {
+	let id = req.params.id;
+
+	if(!ObjectId.isValid(id)) {
+		return res.status(400).send({}) 
+}
+
+	Todo.findById(id)
+	.then((todo) => {
+		if(!todo) return res.status(404).send({});
+		res.send({todo})
+	})
+	.catch((err) => res.status(400).send({}))
 })
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
