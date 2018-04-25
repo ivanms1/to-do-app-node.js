496fb4bdb104adb034f9738d2960c7187fa455fe
Ivan <ivanms1@gmail.com>
2018-04-25 12:46:30 +0900

    GET route added

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find()
+	.then((todos) => {
+		res.send({todos})
+	}, (err) => {
+		res.status(400).send(err);
+	})
+})
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
 module.exports = { app }
